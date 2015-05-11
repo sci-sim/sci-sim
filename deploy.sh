02c892ae66c8bdd619b3b5a80f59895a57398328
caleb lewis <caleb_dre@yahoo.com>
2015-05-10 20:32:54 -0400

    added deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,17 @@
-           
+echo Compressing Files...
+tar -zcvf latest.tar.gz ./*
+
+echo Copying compressed files to server...
+scp latest.tar.gz root@104.131.26.12:/var/www/sci
+
+echo Executing commands on the server...
+ssh root@104.131.26.12 <<'ENDSSH'
+	cd /var/www/sci
+	tar -zcvf archive.tar.gz src/* src/.git
+	cd src
+	rm -rf ./*
+	rm -rf src/.git
+	tar -zxvf ../latest.tar.gz
+ENDSSH
+
+echo Done!           
