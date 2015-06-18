f408179ae90eb45a3df13ce77092f798d18dea0d
Caleb Lewis <caleb_dre@yahoo.com>
2015-06-18 19:10:53 -0400

    added showList to DOMHelper

--- scisim/static/js/Utils/DOMHelper.js
+++ scisim/static/js/Utils/DOMHelper.js
@@ -3,3 +3,15 @@ var DOMHelper = function(){};
 DOMHelper.prototype.iterateContentSections = function(func){
 	$.each($('.page-section'), func);
 };
+
+DOMHelper.prototype.showList = function(list){
+	var html = "<ul class='list'>";
+	
+	for (var i = 0; i < list.length; i++) {
+		var element = list[i];
+		
+		html += "<li class='list-item'>"+element+"</item>";
+	}
+		
+	html += "</ul>";
+};
