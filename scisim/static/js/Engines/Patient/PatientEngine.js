cc542daba5a24d79c4fa602eddee8de3e6819ed7
Michael Parque <mparque3@gatech.edu>
2015-06-18 05:26:21 -0400

    Integrated logging changes into PatientEngine. ChoiceLogManager now takes in a choiceInfo object and logs information about a choice Action String now populated within ChoiceLogManager LabNotebookLogManager tabled for now. Maybe we will want to make this more specific in the future. Fixes made to PatientEngine to allow it to work. 	BUG: Popup windows don't behave correctly.

--- scisim/static/js/Engines/Patient/PatientEngine.js
+++ scisim/static/js/Engines/Patient/PatientEngine.js
@@ -2,47 +2,48 @@
 var PatientEngine = function(){
 	storageHelper.initJsonArray('visited_pages');
 	this.patientManager = new PatientManager();
-	
+
 	this.renderPage(storageHelper.get("first_page_id"));
+	this.choiceLogger = new ChoiceLogManager();
 };
 
 PatientEngine.prototype.renderPage = function(page_id){
 	var that = this;
-	
+
 	if($.inArray(page_id, storageHelper.getJson('visited_pages')) > -1){
 		this.restorePage(page_id);
 		return;
 	}
-	
+
 	storageHelper.appendJsonArray('visited_pages', page_id);
-	
+
 	api.getPage(page_id).done(function(response){
 		if($.isEmptyObject(response)){
 			throw "this page doesn't exist:";
 		}
-		
+
 		var pageContext = response;
 		var patient = that.patientManager.discover();
-		
+
 		pageContext['is_popup'] = false;
-		
+
 		$.map(response.page_modifiers, function(val, i){
 			if($.inArray("popup_window", Object.values(val)) > 0){
 				pageContext['is_popup'] = true;
 			}
 		});
-		
-		var directiveContext = new PatientPageDirectiveApplicator(pageContext.page_actions, pageContext.page_modifiers, patient).getContext(); 
-		
+
+		var directiveContext = new PatientPageDirectiveApplicator(pageContext.page_actions, pageContext.page_modifiers, patient).getContext();
+
 		$.extend(pageContext, directiveContext);
 		$.extend(pageContext, renderer.composePage(pageContext));
-		
+
 		if(patient !== null){
-			$.extend(pageContext, {"patient": patient});	
+			$.extend(pageContext, {"patient": patient});
 		}
-		
+
 		chain.add(pageContext);
-		
+
 		that.applyListeners();
 	});
 };
@@ -50,11 +51,11 @@ PatientEngine.prototype.renderPage = function(page_id){
 PatientEngine.prototype.restorePage = function(page_id){
 	var context = chain.findById(page_id);
 	var patient = context.patient || false;
-	
+
 	new PatientPageDirectiveApplicator(context.page_actions, context.page_modifiers, patient);
-	
-	$.extend(context, renderer.composePage(context));	
-	
+
+	$.extend(context, renderer.composePage(context));
+
 	this.applyListeners();
 };
 
@@ -72,13 +73,13 @@ PatientEngine.prototype.onContinueClick = function(){
 PatientEngine.prototype.onSubmitButtonClick = function(e){
 	e.stopImmediatePropagation();
 	var $inputs = $('input'),
-		choicesMade = [],
-		context = chain.getActivePage();
-	
+		context = chain.getActivePage(),
+		destination = "";
+
 	for (var i = 0; i < $inputs.length; i++) {
 		var input = $inputs.eq(i);
-		var obj = {};
-		
+		var choiceInfo = {};
+
 		if(input.val().length === 0){
 			// TODO: extract this to a form helper class
 			input.addClass("has-danger");
@@ -86,33 +87,32 @@ PatientEngine.prototype.onSubmitButtonClick = function(e){
 			return;
 		}
 
-		obj['value'] = input.val();
-		obj['id'] = input.data("choice-id");
-		
-		var destination = context.goes_to_page || false;
+		destination = context.goes_to_page || false;
 		if(!destination) destination = input.data("destination");
 
-		obj['action_string'] = getActionString(obj['value'], obj['id'], context.id);
+		choiceInfo.choice = input.val();
+		choiceInfo.choice_id = input.data("choice-id");
+		choiceInfo.page_id = page_id;
 
-		choicesMade.push(obj);
-		
+		this.choiceLogger.logChoice(choiceInfo);
+
 		//	TODO: this is just a quick fix. what if the request fails?
 		var loggableString = "";
-		
+
 		if(context.hasOwnProperty("patient")){
-			loggableString += context.patient.name + ": ";	
+			loggableString += context.patient.name + ": ";
 		}else{
 			loggableString += "Question: " + input.prev().text() + "You said: ";
 		}
-		
+
 		loggableString += input.val();
 		labnotebook.add(loggableString.replace("}", ""));
-		
+
 		if(context.hasOwnProperty('patient')) context.patient.choices.push(loggableString);
 	};
-	
+
 	chain.updateContext(context);
-	// we're going to construct this so that later we can make a parser that generates insightful xls documents based on these. 
+	// we're going to construct this so that later we can make a parser that generates insightful xls documents based on these.
 	var user_ids = storageHelper.getJson("user_id");
 
 	if(!$.isArray(user_ids)){
@@ -124,10 +124,13 @@ PatientEngine.prototype.onSubmitButtonClick = function(e){
 	for (var i = 0; i < user_ids.length; i++) {
 		for (var j = 0; j < choicesMade.length; j++) {
 			requests.push([context.id, user_ids[i], choicesMade[j].action_string]);
-		};
-	};
+		}
+	}
 
-	this.logActions(requests, destination);	
+	this.logActions(requests, destination);
+	}
+
+	this.changePage(destination);
 };
 
 
@@ -135,42 +138,23 @@ PatientEngine.prototype.onBinaryChoiceClick = function(e){
 	e.stopImmediatePropagation();
 	var $elem = $(e.currentTarget);
 	var context = chain.getActivePage();
-	
 	if($elem.parent().hasClass("disabled")) return;
-	
+
 	var value = $elem.text(),
 		choiceId = $elem.data('choice-id'),
 		destinationId = $elem.data('destination'),
-		action_string = getActionString(value, choiceId, context.id),
-		user_ids = storageHelper.getJson("user_id");
-		
-	var loggableString = "";
-	
-	if(context.hasOwnProperty("patient")){
-		loggableString += context.patient.name + ": ";	
-	}else{
-		loggableString += "Question: " + $('.page-section').last().text() + " You said: ";
-	}
-	
-	loggableString += value;
-	labnotebook.add(loggableString.replace("}", ""));
-	
+		choiceInfo = {
+			choice: value,
+			choice_id: choiceId,
+			page_id: context.id
+		};
+
 	storageHelper.appendJsonArray("choices_made", choiceId);
-	
-	if(context.hasOwnProperty('patient')) context.patient.choices.push(loggableString);
-	chain.updateContext(context);
 
-	if(!$.isArray(user_ids)){
-		user_ids = [user_ids];
-	}
+	var loggableString = "";
 
-	var requests = [];
-	for (var i = 0; i < user_ids.length; i++) {
-		requests.push([context.id, user_ids[i], action_string]);
-	};
-
-	if(requests.length > 1){
-		this.logActions(requests, destinationId);
+	if(context.hasOwnProperty("patient"){
+		loggableString += chain.getLastPage().patient+ ": ";
 	}else{
 		var that = this;
 		api.logUserAction.apply(null, requests[0]).then(function (response) {
@@ -179,37 +163,39 @@ PatientEngine.prototype.onBinaryChoiceClick = function(e){
 					that.renderPage(destinationId);
 			  }
 		});
+		loggableString += "Question: " + $('.page-section').last().text() + " You said: ";
 	}
-	
+
+	loggableString += value;
+	labnotebook.add(loggableString.replace("}", ""));
+
+	this.choiceLogger.logChoice(choiceInfo);
+
+	var choices_made = JSON.parse(localStorage.getItem("choices_made"));
+	choices_made.push(choiceId);
+	localStorage.setItem("choices_made", JSON.stringify(choices_made));
+
 	$elem.parent().addClass("disabled");
+
+	this.changePage(destinationId);
 };
 
 PatientEngine.prototype.restoreLast = function(){
 	var context = chain.getLastPage();
-	this.changePage(context.id);	
+	this.changePage(context.id);
 };
 
 PatientEngine.prototype.disableChoices = function() {
 	var choices = $('.choice');
 	for (var i = 0; i < choices.length; i++) {
 		choices.eq(i).addClass("disabled");
-	};
+	}
 };
 
-function getActionString(action, choice_id, page_id){
-	return "Choice made: "+ action + " on the choice with id: " + choice_id + " on page: " + page_id;
-}
-
-PatientEngine.prototype.logActions = function(data, destination) {
-	var that = this;
-
-	api.aggregateRequests(api.logUserAction, data).then(function(){
-
-		loader.hide();
-		if(destination){
-			storageHelper.store('last_page_id', chain.getActivePage().id);
-			
-			that.renderPage(destination); // assume that that's all we need to do on the page	
-		}
-	});
+PatientEngine.prototype.changePage = function(destination) {
+	this.choiceLogger.flushLog();
+	if (destination) {
+		localStorage.setItem('last_page_id', chain.getActivePage().id);
+		this.renderPage(destination);
+	}
 };
