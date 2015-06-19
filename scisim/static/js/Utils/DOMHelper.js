715417c5dfd4a2bc9a3457233de75ac6a3166cef
caleb lewis <caleb_dre@yahoo.com>
2015-06-18 23:20:24 -0400

    added attachment for DOMHelper

--- scisim/static/js/Utils/DOMHelper.js
+++ scisim/static/js/Utils/DOMHelper.js
@@ -14,4 +14,25 @@ DOMHelper.prototype.showList = function(list){
 	}
 		
 	html += "</ul>";
+	
+	this.attachUnderHeader(html);
+};
+
+DOMHelper.prototype.attachUnderHeader = function (html) {
+	var $sections = $('.page-section');
+	var $target = null;
+	
+	for (var i = 0; i < $sections.length; i++) {
+		var $element = $sections.eq(i);
+		if ($element.next().hasClass('page-section')) {
+			$target = $element;
+			break;
+		}
+	}
+	
+	if ($target) {
+		$target.append(html);
+	} else {
+		$sections.eq(0).append(html);
+	}
 };
