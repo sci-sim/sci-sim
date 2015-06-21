c5dae7c256de1fc76d9ec19e578a65b0f02438f3
Michael Parque <mparque3@gatech.edu>
2015-06-21 01:06:14 -0400

    Fixed typo.

--- scisim/static/js/Services/PageRenderer.js
+++ scisim/static/js/Services/PageRenderer.js
@@ -44,7 +44,7 @@ PageRenderer.prototype.composePage = function(context) {
 	 			break;
 	 		case "binary":
 	 			templateType = "binary_choice";
-	 			context['hasBinarscmpgmymyy'] = true;
+	 			context['hasBinary'] = true;
 	 			break;
 	 		case "prompt":
 	 			templateType = "prompt_choice";
