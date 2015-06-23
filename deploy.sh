d341ded058c0814987779e848e9eb63c30387c3a
caleb lewis <caleb_dre@yahoo.com>
2015-06-22 23:43:05 -0400

    fixed deploy script issue

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ ssh root@104.131.26.12 <<'ENDSSH'
 	rm -rf src/.git
 	tar -zxvf ../latest.tar.gz
 	pip install -r requirements
-	python db_create
+	python db_create.py
 	python sim_parser.py new_sim_template.txt
 ENDSSH
 
